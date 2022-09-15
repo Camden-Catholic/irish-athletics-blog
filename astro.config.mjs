2dd345d73f6db6111fbe81eabcd68ddd18d95c07
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-09-15 11:51:01 -0400

    website build

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(), sitemap(), svelte(), react()],
-  // site: '',
+  site: 'https://camden-catholic.github.io',
   base: "/"
 });
