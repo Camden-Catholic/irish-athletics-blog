3361a5aa7d75d4ce1e45f323024cd6ba5ef45d97
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-04 13:33:43 -0400

    use link prefetcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from 'astro-robots-txt';
 import sitemap from "@astrojs/sitemap";
-
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), sitemap(), svelte(), react()],
+  integrations: [tailwind(), robotsTxt(), sitemap(), svelte(), react(), prefetch()],
   site: 'https://camden-catholic.github.io',
   base: "/irish-athletics-blog/"
 });
